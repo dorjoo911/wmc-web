c6aaab4a0737dc2b80ebfbde28a46c4b4af51777
mike <erdene.qa@gmail.com>
2023-01-26 00:48:54 -0500

    first dep4

--- api/serve.js
+++ api/serve.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
+// const express = require("express");
+// const cors = require("cors");
+// const app = express();
 
-const dotenv = require("dotenv");
-dotenv.config();
+// const dotenv = require("dotenv");
+// dotenv.config();
 
-app.use(cors());
-app.get("/api/test", (req, res) => {
-  res.json(" hello world: " + Date.now());
-});
+// app.use(cors());
+// app.get("/api/test", (req, res) => {
+//   res.json(" hello world: " + Date.now());
+// });
 
-if (process.env.API_PORT) {
-  app.listen(process.env.API_PORT);
-}
+// if (process.env.API_PORT) {
+//   app.listen(process.env.API_PORT);
+// }
 
-module.exports = app;
+// module.exports = app;
